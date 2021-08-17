1d8fa7db21ff8a4c9fa204a300a51c8a5212ed52
Zak Stamps <theoneandonlyzako@gmail.com>
2021-08-16 21:20:23 -0700

    added footer names

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2021 by Dylan Trecker</div>
+      <div className="container">&copy;2021 by Dylan T, Dylan R, Michael C, Ben G, Zak S</div>
     </footer>
   );
 };
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -51,7 +51,7 @@ const Profile = () => {
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : "your"} profile.
+         {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
         {userParam && (
           <button className="btn ml-auto" onClick={handleClick}>
