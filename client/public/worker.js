79a5d493d2d4d74aaa7eca26a445cfc7e7c7f87c
Zak Stamps <theoneandonlyzako@gmail.com>
2021-08-17 17:33:11 -0700

    HTTPS issue backing up

---
+++ client/public/worker.js
@@ -0,0 +1,52 @@
+const APP_PREFIX = 'kiss-anime';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = "data-cache-" + VERSION;
+
+const FILES_TO_CACHE = [
+  "/",
+  "./index.html"
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+    var cacheWhitelist = ['pwa-task-manager'];
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
