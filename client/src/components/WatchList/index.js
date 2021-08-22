503e50e5867fbc8a2fc3d6f636618feff2504275
Emcee <mjchambersjr@gmail.com>
2021-08-21 19:30:16 -0700

    style MyAnime

--- client/src/components/WatchList/index.js
+++ client/src/components/WatchList/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-
+import '../WatchList/styles.css'
 
 const WatchList = ({ username, animes }) => {
     if (!animes || !animes.length) {
-      return <p className="bg-dark text-light p-3">{username}, go watch some animes!</p>;
+      return <p className="bg-dark text-light p-3 noAnimeWarning">{username}, go watch some animes!</p>;
     }
   
     return (
       <div>
+        <h5>My Anime List</h5>
         {animes.map(anime => (
-          <div className="btn w-100 display-block mb-2" key={anime._id}>
+          <div className="btn w-100 display-block mb-2 myAnimeBtn" key={anime._id}>
             {anime.animeText} {anime.animeEpisodes}
           </div>
         ))}
