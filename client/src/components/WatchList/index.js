398f49572255473009c366dda24b96796a867e71
Zak Stamps <77663546+theoneandonlyzako@users.noreply.github.com>
2021-08-22 22:58:09 -0700

    adding an editable text field instead of a drop down

--- client/src/components/WatchList/index.js
+++ client/src/components/WatchList/index.js
@@ -3,31 +3,59 @@ import React from "react";
 import '../WatchList/styles.css'
 
 const WatchList = ({ username, animes }) => {
-    if (!animes || !animes.length) {
-      return <p className="bg-dark text-light p-3 noAnimeWarning">{username}, go watch some animes!</p>;
-    }
+  if (!animes || !animes.length) {
+    return <p className="bg-dark text-light p-3 noAnimeWarning">{username}, go watch some animes!</p>;
+  }
 
-  const episodeOptions = (number) => {
-    var epList = [];
-    for (let i = 1; i <= number; i++) {
-      epList.push(i);
-    }
-    return epList;
+  // function addEpisode() {
+  //   var text = (this)
+  //     .text()
+  //     .trim();
+  //   var textInput = ("<textarea>")
+  //     .addClass("form-control")
+  //     .val(text);
+
+  //   (this).replaceWith(textInput);
+  //   textInput.trigger("focus");
+  //   console.log("clicked!");
+
+  // }
+
+  const saveEpisode = () => {
+    // var epis = (this)
+    //   .text()
+    //   .trim();
+    // var textInput = ("<textarea>")
+    //   .val(epis);
+
+    // (this).replaceWith(textInput);
+    // textInput.trigger("focus");
+    console.log("clicked!");
+
   };
 
- return (
+  const newEpisode = () => {
+    // get the textarea's current value/text
+    var text = (this)
+      .val()
+      .trim();
+
+    // replace p element
+    var taskP = ("<textarea>")
+      .text(text);
+
+    // replace textarea with p element
+    (this).replaceWith(taskP);
+  }
+
+  return (
     <div>
-     <h5>My Anime List</h5>
+      <h5>My Anime List</h5>
       {animes.map((anime) => (
         <div className="btn w-100 display-block mb-2 myAnimeBtn" key={anime._id}>
           {anime.animeText} {anime.animeEpisodes}
-          <select className="select-board-size">
-            {episodeOptions(anime.animeEpisodes).map((value) => (
-              <option key={value} value={value}>
-                {value}
-              </option>
-            ))}
-          </select>
+          <label>Current Episode</label>
+          <textarea onFocus={saveEpisode} onBlur={newEpisode} id="modalTaskDescription"></textarea>
         </div>
       ))}
     </div>
